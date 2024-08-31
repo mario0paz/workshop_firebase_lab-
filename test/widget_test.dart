94de159c803970808b81110ebf74ad87c0dfd9ba
Mario Paz <mariopaz5399@gmail.com>
2024-08-31 17:18:16 -0600

    ,

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Basic rendering', (tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (context) => ApplicationState(),
@@ -15,7 +15,7 @@ void main() {
       ),
     );
 
-    // Verify that our counter starts at 0.
+    
     expect(find.text('Firebase Meetup'), findsOneWidget);
     expect(find.text('January 1st'), findsNothing);
   });
